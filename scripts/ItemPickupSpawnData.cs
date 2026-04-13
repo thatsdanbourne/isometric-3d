fb7c1e347bb2827da7e93db9f525bbe553a4b214
thatsdanbourne <dan311095@hotmail.co.uk>
2026-04-13 01:08:26 +0100

    introduced `ItemPickupSpawnData` struct to define item pickup properties, laying groundwork for consistent state handling and multiplayer synchronization

---
+++ scripts/ItemPickupSpawnData.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public struct ItemPickupSpawnData
+{
+	public ulong PickupId;
+	public string ItemId;
+	public int Count;
+	public Vector3 Position;
+	public Vector3 InitialVelocity;
+	public float InitialVerticalVelocity;
+}
